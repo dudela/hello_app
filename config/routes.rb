0dfd76a9a43d985e05f203318e8eb6aad55ec9ca
CD065208 <chandrakanth.dudela@cerner.com>
2018-12-20 12:42:43 -0500

    Changed root url

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   resources :microposts
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'users#index'
+  root 'static_pages#home'
 end
